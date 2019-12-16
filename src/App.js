5afd6480257755eca1a0193c814bd29ef8e05d82
codypearce <codyapearce@gmail.com>
2019-12-15 21:00:19 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Text } from "react-native";
+
+export default class App extends React.Component {
+  render() {
+    return <Text>Hello world</Text>;
+  }
+}
